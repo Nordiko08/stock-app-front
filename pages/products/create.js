d82842bd0344c11aa1905f69cfe7c51a60867f83
Nordiko08 <ayalaemanuel68@gmail.com>
2024-02-12 15:30:41 -0300

    ahora si ultimo commit

--- pages/products/create.js
+++ pages/products/create.js
@@ -1,55 +1,60 @@
 import { useState } from "react"
 
-const initialState = { name: '', price: 0 }
+const initialState = { name: "", price: 0 }
 function Create() {
-    const [product, setProduct] = useState(initialState)
-    const handleChange = (e) => {
-        const inputValue = e.target.value
-        const inputName = e.target.name
+  const [product, setProduct] = useState(initialState)
 
-        setProduct({
-            ...product,
-            [inputName]: inputValue,
-        })
-    }
-    const handleClick = (e) => {
-        e.preventDefault()
+  const handleChange = (e) => {
+    const inputValue = e.target.value
+    const inputName = e.target.name
+    setProduct({
+      ...product,
+      [inputName]: inputValue,
+    })
+  }
 
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`, {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify(product),
-        }) 
-        .then((res) => res.json())
-           .then((data) => {
-            setProduct(initialState)
-            console.log('Producto creado con éxito')
-        }) .catch((err) => {
-            console.log({err})
-        })
-    }
+  const handleClick = (e) => {
+    e.preventDefault()
+
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(initialState)
+        console.log("Producto creado con éxito")
+      })
+      .catch((err) => {
+        console.log({ err })
+      })
+  }
+
   return (
     <>
-    <div>
+      <div>
         <h1>
-            <u>Crear nuevo producto</u>
+          <u>Crear nuevo producto</u>
         </h1>
         <form>
-            <input 
-            type="text" 
-            name="name" 
-            value={product.name} 
-            onChange={handleChange} />
-            <input 
-            type="number" 
-            name="price" 
-            value={product.price} 
-            onChange={handleChange}/>
-            <button onClick={handleClick}>Crear producto</button>
+          <input
+            type="text"
+            name="name"
+            value={product.name}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="price"
+            value={product.price}
+            onChange={handleChange}
+          />
+          <button onClick={handleClick}>Crear producto</button>
         </form>
-    </div>
+      </div>
     <style jsx>
         {`
         form {
