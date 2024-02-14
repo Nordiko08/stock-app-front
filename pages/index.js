30799a1a039ca9a84889d2e7c9ee16ccff4d0af0
Nordiko08 <ayalaemanuel68@gmail.com>
2024-02-14 17:30:24 -0300

    tercer commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hola mundo</h1>
-      <Link href="/products/create">Crear producto v3</Link>
+      <Link href="/products/create">Crear producto v4</Link>
     </div>
   )
 }
